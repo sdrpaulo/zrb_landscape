9f7a72a6cb9ac58dffd59ec2e85076ece39c7bbe
Paulo da Silva <dasilvapaulo@aol.com>
2020-11-19 18:26:20 -0500

    Third Test

--- js/site.js
+++ js/site.js
@@ -33,7 +33,82 @@ const site = {
     }))
   },
 
+  initializeCarousel() {
+
+    const secondarySlider = new Splide('#secondary-slider', {
+      rewind      : true,
+      fixedWidth  : 100,
+      fixedHeight : 64,
+      isNavigation: true,
+      gap         : 10,
+      focus       : 'center',
+      pagination  : false,
+      cover       : true,
+      drag        : true,
+      arrows      : false,
+      breakpoints : {
+        '374': {
+          fixedWidth  : 25,
+          fixedHeight : 25,
+          gap: 5,
+        },
+        '413': {
+          fixedWidth  : 30,
+          fixedHeight : 30,
+          gap: 5,
+        },
+        '600': {
+          fixedWidth  : 35,
+          fixedHeight : 35,
+          gap: 5,
+        },
+        '1023': {
+          fixedWidth  : 65,
+          fixedHeight : 65,
+          gap: 5,
+        },
+        '1199': {
+          fixedWidth  : 90,
+          fixedHeight : 90,
+          gap: 5,
+        }
+      }
+    }).mount();
+
+    const primarySlider = new Splide('#primary-slider', {
+      type        : 'fade',
+      rewind      : true,
+      fixedWidth  : 800,
+      heightRatio : 0.6,
+      pagination  : false,
+      arrows      : true,
+      drag        : true,
+      cover       : true,
+      breakpoints : {
+        '374': {
+          fixedWidth  : 275,
+          heightRatio: 0.8,
+        },
+        '413': {
+          fixedWidth  : 340,
+          heightRatio: 0.8,
+        },
+        '600': {
+          fixedWidth  : 380,
+          heightRatio: 0.8,
+        },
+        '1023': {
+          fixedWidth  : 650,
+          heightRatio: 0.8,
+        },
+      }
+    })
+
+    primarySlider.sync(secondarySlider).mount();
+  },
+
   init() {
+    this.initializeCarousel();
     this.displayMobileMenu();
     AOS.init({duration: 1500});
   }
